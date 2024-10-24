544eb66d0b085371bbf40a570816a79ad8e0ddff
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-10-24 21:08:59 +0530

    add dbContext

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add DBContext
+builder.Services.AddDbContext<ApplicationDBContext>(Options => {
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
