30830b647afce223782c36f5ec0cf431dfb3b437
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-11-05 23:34:51 +0530

    Install Identity

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 using api.Data;
 using api.Interfaces;
+using api.Models;
 using api.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +31,40 @@ builder.Services.AddSwaggerGen();
 // 4. Add AutoMapper
 builder.Services.AddAutoMapper(typeof(Program));
 
+//7. Add Identity Service
+builder.Services.AddIdentity<AppUser, IdentityRole>(option =>
+{
+    option.Password.RequiredLength = 8;
+    option.Password.RequireUppercase = true;
+    option.Password.RequireLowercase = true;
+    option.Password.RequireNonAlphanumeric = true;
+    option.Password.RequireDigit = true;
+}).AddEntityFrameworkStores<ApplicationDBContext>();
+
+//8. Add Scheme
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+    options.DefaultChallengeScheme =
+    options.DefaultForbidScheme =
+    options.DefaultScheme =
+    options.DefaultSignInScheme =
+    options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+        )
+    };
+});
+
 // 5. Add Repositories
 builder.Services.AddScoped<IStockRepository, StockReposiory>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
@@ -41,6 +79,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//9. add  then database mig,update
+app.UseAuthentication();
+app.UseAuthorization();
 
 //3. add Mapcontrollers
 app.MapControllers();
