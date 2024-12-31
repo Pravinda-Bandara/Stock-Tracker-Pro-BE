9e96b71ec765720b0a3d4fde4cada9e1abe0c986
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-12-31 20:32:37 +0530

    fix createdBy

--- Dtos/Comment/CommentDto.cs
+++ Dtos/Comment/CommentDto.cs
@@ -6,7 +6,7 @@
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime CreateOn { get; set; } = DateTime.Now;
-        public string CreatedBy { get; set; } =string.Empty;
+        public string AppUser { get; set; } =string.Empty;
         public int? StockId { get; set; }
     }
 }
